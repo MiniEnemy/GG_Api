583c9ba026d3eb29c91cba0bf220d5c85a59a223
MiniEnemy <home.minj@gmail.com>
2024-11-07 02:33:59 +0545

    s

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 
 const express = require('express');
-const connectDB = require("../API/db/connect");
+const connectDB = require("./db/connect");
 const app = express();
 
 const port = process.env.PORT || 5000;
